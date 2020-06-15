d61e845feda8fb7c0e92f3d4693ddc92e0595ef2
Shirlei Machado <schiufreitas@gmail.com>
2020-06-15 17:04:43 -0300

    Rotas, componetes e páginas criadas

---
+++ src/Components/AreaPessoal/TelaInicialPage/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+
+export const TelaToda = styled.div`
+  margin: 0;
+  padding: 0%;
+  background-color: #d0d0d0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const DivInterna = styled.div`
+  width: 360px;
+  box-sizing: border-box;
+  height: 640px;
+  margin: 5px;
+  border: 1px solid black;
+  background-color: #e8222e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
