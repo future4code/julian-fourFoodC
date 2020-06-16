cb36971ede0c3b47e0624e746397d9d5b0a55325
Tais Itoo <tais2001@gmail.com>
2020-06-16 10:19:07 -0300

    cadastro

--- src/components/pages/Cadastro/index.js
+++ src/components/pages/Cadastro/index.js
@@ -1,11 +1,101 @@
 import React from 'react';
+import logo from '../../../img/logo-future-eats-red.png'
+import TextField from '@material-ui/core/TextField';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components'
+
+const TelaToda = styled.div`
+  margin: 0;
+  padding: 0%;
+  background-color: #d0d0d0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const DivInterna = styled.div`
+  width: 360px;
+  box-sizing: border-box;
+  height: 640px;
+  margin: 5px;
+  border: 1px solid black;
+  background-color: white;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column; 
+
+`
+const BotaoInsc = styled.button`
+color: black;
+background-color: #e8222e;
+padding: 12px;
+font-size: 15px;
+font-weight:bold;
+border: none;
+
+:hover {
+    background-color: #b90510;
+}
+` 
 
 const Cadastro = () => {
-  return (
-    <div>
+  const history = useHistory();
 
-    </div>
+  const goToHome = () => {
+    history.push("/home")
+    
+}
+
+  return (
+    <TelaToda>
+    <DivInterna>
+      <img src={logo} alt="iFuture"/>
+      <TextField
+          id="outlined-helperText"
+          label="Nome*"
+          defaultValue="Nome e sobrenome"
+          variant="outlined"
+          
+
+        />
+      <TextField
+          id="outlined-helperText"
+          label="E-mail*"
+          defaultValue="email@email.com"
+          variant="outlined"
+
+        />
+              <TextField
+          id="outlined-helperText"
+          label="CPF*"
+          defaultValue="000.000.000-00"
+          variant="outlined"
+          
+
+        />
+      <TextField
+          id="outlined-helperText"
+          label="Senha*"
+          defaultValue="Mínimo 6 caracteres"
+          variant="outlined"
+
+        />
+         <TextField
+          id="outlined-helperText"
+          label="Confirmar*"
+          defaultValue="Confirme a senha anterior"
+          variant="outlined"
+
+        />
+    <BotaoInsc onClick = { goToHome } > Criar </BotaoInsc> 
+    </DivInterna>
+  </TelaToda>
   );
 }
+
 
 export default Cadastro;
--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -1,10 +1,81 @@
 import React from 'react';
+import logo from '../../../img/logo-future-eats-red.png'
+import TextField from '@material-ui/core/TextField';
+
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components'
+
+const TelaToda = styled.div`
+  margin: 0;
+  padding: 0%;
+  background-color: #d0d0d0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const DivInterna = styled.div`
+  width: 360px;
+  box-sizing: border-box;
+  height: 640px;
+  margin: 5px;
+  border: 1px solid black;
+  background-color: white;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column; 
+
+`
+const BotaoInsc = styled.button`
+color: black;
+background-color: #e8222e;
+padding: 12px;
+font-size: 15px;
+font-weight:bold;
+border: none;
+
+:hover {
+    background-color: #b90510;
+}
+` 
 
 const Login = () => {
-  return (
-    <div>
+  const history = useHistory();
 
-    </div>
+  const goToHome = () => {
+    history.push("/home")
+};
+
+const goToCadastro = () => {
+  history.push("/cadastro")
+};
+
+  return (
+    <TelaToda>
+    <DivInterna>
+      <img src={logo} alt="iFuture"/>
+      <TextField
+          id="outlined-helperText"
+          label="Login"
+          defaultValue="email@email.com"
+          variant="outlined"     
+
+        />
+      <TextField
+          id="outlined-helperText"
+          label="Senha"
+          defaultValue="Mínimo 6 caracteres"
+          variant="outlined"
+
+        />
+    <BotaoInsc onClick = { goToHome } > Entrar </BotaoInsc> 
+    <p>Não possui cadastro?<p onClick = { goToCadastro } >Clique aqui</p></p>
+    </DivInterna>
+  </TelaToda>
   );
 }
 
